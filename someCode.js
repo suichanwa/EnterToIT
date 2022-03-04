dd13921a2f50f36e32fb4e89c1b2c4a03bbfd79f
suichanwa <91247676+suichanwa@users.noreply.github.com>
2022-03-04 21:16:47 +0200

    Update someCode.js

--- someCode.js
+++ someCode.js
@@ -1,30 +1,129 @@
 let sum = 0;
 let arr = [1,2,3];
+let result = getInt([1, 2, 3], [2, 3, 4], [4, 3, 2]);
 let arrWithLetters = ['a', 'b', 'c'];
 let numbs = [-1,2,3,4,-5, 7, 8,9];
 let nums = [2,3,4,5,6]; 
 let date = new Date();
-let elem   = document.querySelector('#elem');
-//хуйня 
+let elem  = document.querySelector('#elem');
+let divs = document.querySelectorAll('div');
+let elem2 = document.querySelector('#elem2');
 let eve = nums.every(elem => elem %  2 == 0);
 let but = document.querySelector('#button');
-but.addEventListener('click', function(){
-	console.log('go fuck off');
-})
-but.addEventListener('click', function() {
-	elem.innerHTML = '!!!';
+let foc = document.querySelector('#focus');
+let changeEvent = document.querySelector('#changeEvent');
+let input = document.querySelector('#input');
+let lii = document.querySelector('#checked');
+let modBut = document.querySelector('#elem');
+let div = document.querySelector('div');
+
+
+//clickInThisDiv
+
+
+div.addEventListener('click', function(event) {
+	if (event.target.matches('div')) {
+		alert('clicked on div');
+	}
+	if (event.target.matches('p')) {
+		alert('click on p');
+	}
 });
-console.log(but);
 
-
-function consoleLogs(){
-    console.log(['a', ['b', 'c', 'd'], ['e', 'f', ['g', ['j', 'k']]]]);  
-    console.log(sum);  
-    console.log(numbs.filter(elem => elem > 0));
-    console.log(eve);
-	console.log(date.getFullYear());
+function modButtons(){
+	modBut.addEventListener('click', function(event) {
+		if (event.ctrlKey) {
+			console.log('нажат Ctrl');
+		}
+		
+		if (event.altKey) {
+			console.log('нажат Alt');
+		}
+		
+		if (event.shiftKey) {
+			console.log('нажат Shift');
+		}
+	});
 }
 
+function WorkWuithLists(){
+
+	lii.addEventListener('click', function(event){
+		console.log(event.target); // выведет наш абзац
+		console.log(this);   
+	});
+
+	elem.addEventListener('click', function(event) {
+		console.log(event.target); // выведет наш абзац
+		console.log(this);         // выведет наш див
+	});
+
+	document.addEventListener('mousemove', function(event) {
+		elem.innerHTML = event.clientX + ' : ' + event.clientY;
+	});
+
+	input.addEventListener('input', function(){
+		console.log(this.value);
+	});
+}
+
+//event change
+
+changeEvent.addEventListener('changeEvent', function(){
+	console.log(this.value);
+})
+
+//radioButtons
+
+function radioButs(){
+	let radios = document.querySelectorAll('input[type="radio"]');
+	let button = document.querySelector('#button');
+
+	button.addEventListener('click', function(){
+		for(let radio of radios){
+			if(radio.checked){
+				console.log(radio.value);
+			}
+		}
+	})
+}
+
+
+//disableEnable
+function disEnbl(){
+	let elem = document.querySelector('#elem');
+	let button = document.querySelector('#button');
+
+	button.addEventListener('click', function(){
+		elem.disable = !elem.disabled;
+	})
+}
+
+
+//remove affter first tap 
+
+function Remove(){
+	let elems = document.querySelectorAll('p');
+
+	for(elem of elems){
+		elem.addEventListener('click', function func(){
+			alert(this.innerHTML);
+			this.removeEventListener('click', func);
+		});
+	}
+}
+
+
+function func() {
+	alert('!!!');
+	this.removeEventListener('click', func);
+}
+
+for (let div of divs) {
+	div.addEventListener('click', function(){
+		this.innerHTML++;
+	});
+}
 
 function func(){
     let sum = 0;
@@ -52,9 +151,6 @@ arrWithLetters.forEach(function(elem){
 arrWithLetters.forEach(function(elem) {
 	sum += elem;
 });
-
-
-
 
 function func(num1, num2, num3, num4, num5) {
 	return num1 + num2 + num3 + num4 + num5;
@@ -92,12 +188,20 @@ function inArray(elem, arr) {
 	return arr.indexOf(elem) !== -1;
 }
 
-// Проверим работу:
-let result = getInt([1, 2, 3], [2, 3, 4], [4, 3, 2]);
-console.log(result); 
+function consoleLogs(){
+    console.log(['a', ['b', 'c', 'd'], ['e', 'f', ['g', ['j', 'k']]]]);  
+    console.log(sum);  
+    console.log(numbs.filter(elem => elem > 0));
+    console.log(eve);
+	console.log(date.getFullYear());
+	console.log(result);
+}
 
-//date
-
-
+//functions_source
 
 consoleLogs();
+Remove();
+disEnbl();
+radioButs();
+modButtons();
+WorkWuithLists()
