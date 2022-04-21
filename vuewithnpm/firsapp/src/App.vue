e272288edcc01d31c2bd89576197c4d42fd49fe9
suichanwa <augustinnegura746@gmail.com>
2022-04-21 19:58:27 +0300

    final

---
+++ vuewithnpm/firsapp/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    
+    <!-- Applying Uppercase filter -->
+    <h1>{{ name| uppercase }}</h1>
+  </div>
+</template>
+  
+<!-- Filter defined locally -->
+<script>
+export default {
+  data: function() {
+    return {
+      name: "what the fuck"
+    };
+  },
+  filters: {
+    uppercase: function(value) {
+      return value.toUpperCase();
+    }
+  }
+};
+</script>
--- vuewithnpm/firsapp/src/main.js
+++ vuewithnpm/firsapp/src/main.js
@@ -13,7 +13,7 @@ changeData.age = 345;
 new Vue({
   el: '#app',
   data: {
-      message: 'Hello Vue!',
+      message: 'fu!',
       name: 'tome',
       age: 23
   },
@@ -31,7 +31,14 @@ new Vue({
         name: 'first name',
         age: 34,
         template: '<p>Имя: {{name}}   Возраст {{age}}</p>',
-        
+        visible: true,
+        users:[
+            {name:'Tom', age:22},
+            {name:'Bob', age:25},
+            {name:'Sam', age:28},
+            {name:'Alice', age:26}
+        ],
+        selectedUsers:[],
     },
     methods:{
         welcome: function(){
@@ -52,6 +59,10 @@ new Vue({
         },
         decrese: function(n){
             this.counter = this.count - n;
+        },
+        change: function(){
+            this.$refs.header.innerHTML = "testing";
+            this.$refs.header.value;
         }
     }
 });
@@ -85,3 +96,67 @@ new Vue({
         }
     }
 })
+
+new Vue({
+    el:'#classes',
+    data: {isActive : false}
+})
+
+new Vue({
+    el:'#watch',
+    data: {number: '', result: ''},
+   
+    watch:{
+        number: function(newNumber){
+            if(newNumber > 0);
+                this.factorial(newNumber);
+        }
+    },
+    methods:{
+        factorial: function(newNumber){
+            this.result = 'fac result...';
+            var vm = this;
+            setTimeout(function(){
+                var res = 1;
+                    for(var i = 1; i<=newNumber; i++){
+                        res = res * i;
+                    }
+                    vm.result = 'factorial of number ' + newNumber + ' is ' + res;
+            }, 2000);
+        }
+    }
+})
+
+Vue.component('some-thing',{
+    template:'<h1>fucked </h1>'
+})
+
+new Vue({
+    el:'#_app',
+    data: {
+        user :'name',
+        message : 'somevalue'
+    },
+    methods:{
+        chagne_name: function(){
+            this.name = 'changed';
+        }
+    }
+})
+
+Vue.component('useredit-ed', {
+    props: ["user"],
+    data: function () {
+      return { userName: this.user}
+    },
+    template: '<div><input type="text" v-model="userName" /><p>Name: {{userName}}</p></div>'
+});
+
+Vue.component('message-container',{
+    props:'message that will be showed',
+    template: '<h1>props testing</h1>'
+})
+
+Vue.filter('uppercase', function (value) {
+    return value.toUpperCase();
+  });
