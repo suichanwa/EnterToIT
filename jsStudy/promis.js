9467b5cde7fc91a4b5cdfac94185e923642e4878
suichanwa <augustinnegura746@gmail.com>
2022-05-22 22:48:50 +0300

    promis

--- jsStudy/promis.js
+++ jsStudy/promis.js
@@ -1,24 +1,38 @@
-let a = 3
+function times() {
+        setTimeout(() => {
+        console.log("request");
 
-setTimeout(() => {
-    console.log("request");
+        const data = {
+            one : 'test', 
+            another: 'af'
+        }
 
-    const data = {
-        one : 'test', 
-        another: 'af'
-    }
-
-    console.log('data resive');
-    setTimeout(() => {
-        data.one = 'not test'
-       console.log(data);
-    }, 1000)
-}, 2000)
+        console.log('data resive');
+        setTimeout(() => {
+            data.one = 'not test'
+        console.log(data);
+        }, 1000)
+    }, 2000)
+}
 
 const p = new Promise(function(resolve, reject){
    setTimeout(() => {
     console.log('data resive');
-
-    
+    const another = {
+        server: 'servername',
+        port: 2000,
+        status: 'working'
+    }
+    resolve(another);
    },1000) 
+})
+
+p.then((another => {
+   return new Promise(function(resolve, reject){
+    another.modified = true; 
+    resolve(another);
+   }, 2000) 
+
+})).then(clientData => {
+    console.log('data', clientData);
 })
