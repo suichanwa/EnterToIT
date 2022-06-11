33efe947118e015d6d2e5ffd720ffb023e22336c
suichanwa <augustinnegura746@gmail.com>
2022-06-11 11:49:28 +0300

    mcvrealisation

---
+++ php/mvcFrameWork/core/Track.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace core;
+
+class Track{
+    private $controller;
+    private $action;
+    private $params;
+    
+    public function __construct($controller, $action, $params){
+        $this->controller = $controller;
+        $this->action = $action;
+        $this->params = $params;
+    }
+
+    public function __get($property){
+        return $this->$property;
+    }
+
+    public function getTrack($routes, $uri){
+
+        foreach ($routes as $route) {
+            if($route->path == $uri){
+                $this->controller = $route->controller;
+                $this->action = $route->action;
+                $this->params = $route->params;
+                return true;
+            }
+        }
+    }
+}
+
+?>
