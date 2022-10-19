f63539114d8d1f1ec9740b575c3165c9fccbc35a
suichanwa <augustinnegura746@gmail.com>
2022-10-19 09:48:56 +0300

    invtntory

---
+++ inventoryjs/demo.js
@@ -0,0 +1,16 @@
+//create a variable with items and png icon
+
+var items = {
+    "1": {
+        icon: "img/icons/1.png",
+        text: "Item 1"
+    },
+    "2": {
+        icon: "img/icons/2.png",
+        text: "Item 2"
+    }
+}
+
+console.log(JSON.stringify(items));
+
+
---
+++ inventoryjs/index.js
@@ -0,0 +1,2 @@
+var app = require('ui-select');
+
